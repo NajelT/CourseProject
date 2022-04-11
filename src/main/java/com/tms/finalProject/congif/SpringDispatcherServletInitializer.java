f90e66b030f518e2b0958e0e278dea6893fd57c4
Ilja <ilushpajev@gmail.com>
2022-04-11 09:42:45 +0300

    environment setup

---
+++ src/main/java/com/tms/finalProject/congif/SpringDispatcherServletInitializer.java
@@ -0,0 +1,20 @@
+package com.tms.finalProject.congif;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
